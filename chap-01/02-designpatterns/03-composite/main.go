a06803ecb7b9d240409d94b7f304411661c5b207
disharjayantha <dishuj15@gmail.com>
2021-02-01 17:48:22 +0530

    composite design pattern

--- chap-01/02-designpatterns/03-composite/main.go
+++ chap-01/02-designpatterns/03-composite/main.go
@@ -2,18 +2,69 @@ package main
 
 import "fmt"
 
-type person struct {
+type IComposite interface {
+	perform()
+}
+
+type Leaflet struct {
 	name string
 }
 
-func (p *person) printName() {
-	fmt.Println("Name:", p.name)
+func (leaf *Leaflet) perform() {
+	fmt.Println("Leaflet:", leaf.name)
+}
+
+type Branch struct {
+	name     string
+	leafs    []Leaflet
+	branches []Branch
+}
+
+func (branch *Branch) perform() {
+	fmt.Println("Branch:", branch.name)
+
+	for _, leaf := range branch.leafs {
+		leaf.perform()
+	}
+
+	for _, branch := range branch.branches {
+		branch.perform()
+	}
+}
+
+func (branch *Branch) addLeaf(newleaf *Leaflet) {
+	branch.leafs = append(branch.leafs, *newleaf)
+}
+
+func (branch *Branch) addBranch(newbranch *Branch) {
+	branch.branches = append(branch.branches, *newbranch)
+}
+
+func (branch *Branch) getLeaflets() []Leaflet {
+	return branch.leafs
 }
 
 func main() {
-	p := person{
-		name: "Smith",
+	branch1 := &Branch{
+		name: "Branch 1",
 	}
 
-	p.printName()
+	leaf1 := &Leaflet{
+		name: "Leaf 1",
+	}
+
+	leaf2 := &Leaflet{
+		name: "Leaf 2",
+	}
+
+	branch2 := &Branch{
+		name: "Branch 2",
+	}
+
+	branch1.addLeaf(leaf1)
+	branch1.addLeaf(leaf2)
+
+	branch1.addBranch(branch2)
+
+	branch1.perform()
 }
